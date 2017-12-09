d7c0e33e495c5fcffaa88c45fe56a548180893b2
LeevernVasanth17 <31160298+LeevernVasanth17@users.noreply.github.com>
2017-12-09 19:39:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get("/", function(req, res) {
   res.render("index");
 });
 
-app.listen(5000, function(req, res) {
+app.listen(process.env.PORT || 5000, function(req, res) {
   console.log("server started at port 5000");
 });
